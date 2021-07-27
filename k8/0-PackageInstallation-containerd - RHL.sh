50319864b4d5bfc8be0d9584acccac5d6cd55d09
Damindu Liyanage <damindu7@gmail.com>
2021-07-27 05:27:53 -0400

    Create 0-PackageInstallation-containerd - RHL.sh

---
+++ k8/0-PackageInstallation-containerd - RHL.sh
@@ -0,0 +1,184 @@
+# Optimized for RHL
+
+sudo swapoff -a
+sudo nano /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# containerd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
+#EPEL
+sudo yum install epel-release
+sudo yum repolist
+sudo yum repolist | grep epel
+# if needed to enable epel sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E '%{rhel}').noarch.rpm
+
+ sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+# sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
+
+
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+    SystemdCgroup = true
+# cgroup driver
+sudo yum install nano
+sudo nano /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+lsmod | grep br_netfilter
+# sudo modprobe br_netfilter
+
+# enable routes 
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# check ports
+sudo yum install lsof netstat
+sudo lsof -i -P -n | grep LISTEN
+sudo netstat -tulpn | grep LISTEN.
+sudo lsof -i:6443
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+VERSION=1.20.1
+sudo yum install -y kubelet-$VERSION kubeadm-$VERSION kubectl-$VERSION --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+sudo systemctl status kubelet
+
+sudo yum install yum-plugin-versionlock
+sudo yum versionlock kubelet-$VERSION kubeadm-$VERSION kubectl-$VERSION
+sudo yum update
+
+sudo systemctl status kubelet.service 
+sudo systemctl status containerd.service 
+
+
+#Ensure both are set to start when the system starts up.
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
+
+
+wget https://docs.projectcalico.org/manifests/calico.yaml
+nano calico.yaml
+kubeadm config print init-defaults | tee ClusterConfiguration.yaml
+
+#Inside default configuration file, we need to change four things.
+#1. The IP Endpoint for the API Server localAPIEndpoint.advertiseAddress:
+#2. nodeRegistration.criSocket from docker to containerd
+#3. Set the cgroup driver for the kubelet to systemd, it's not set in this file yet, the default is cgroupfs
+#4. Edit kubernetesVersion to match the version you installed in 0-PackageInstallation-containerd.sh
+
+#Change the address of the localAPIEndpoint.advertiseAddress to the Control Plane Node's IP address
+sed -i 's/  advertiseAddress: 1.2.3.4/  advertiseAddress: 10.0.2.8/' ClusterConfiguration.yaml
+
+
+#Set the CRI Socket to point to containerd
+sed -i 's/  criSocket: \/var\/run\/dockershim\.sock/  criSocket: \/run\/containerd\/containerd\.sock/' ClusterConfiguration.yaml
+
+
+#Set the cgroupDriver to systemd...matching that of your container runtime, containerd
+cat <<EOF | cat >> ClusterConfiguration.yaml
+---
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+cgroupDriver: systemd
+EOF
+
+
+sudo kubeadm init \
+    --config=ClusterConfiguration.yaml \
+    --cri-socket /run/containerd/containerd.sock
+
+# if something go wrong do a $kubeadm reset
+
+
+#Before moving on review the output of the cluster creation process including the kubeadm init phases, 
+#the admin.conf setup and the node join command
+
+
+#Configure our account on the Control Plane Node to have admin access to the API server from a non-privileged account.
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#1 - Creating a Pod Network
+#Deploy yaml file for your pod network.
+kubectl apply -f calico.yaml
+
+
+#Look for the all the system pods and calico pods to change to Running. 
+#The DNS pod won't start (pending) until the Pod network is deployed and Running.
+kubectl get pods --all-namespaces
+
+
+#Gives you output over time, rather than repainting the screen on each iteration.
+kubectl get pods --all-namespaces --watch
+
+
+#All system pods should be Running
+kubectl get pods --all-namespaces
+
+
+#Get a list of our current nodes, just the Control Plane Node/Master Node...should be Ready.
+kubectl get nodes 
+ 
