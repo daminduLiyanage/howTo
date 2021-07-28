5b616a0f0f590c3af5bc9ce299622d73fb499c08
Damindu Liyanage <damindu7@gmail.com>
2021-07-27 23:50:05 -0400

    Update 2-CreateNodes-containerd - RHL.sh

--- k8/2-CreateNodes-containerd - RHL.sh
+++ k8/2-CreateNodes-containerd - RHL.sh
@@ -1,4 +1,125 @@
- 
+# Optimized for RHL
+
+# check LAN ip address
+ip address
+
+sudo swapoff -a
+sudo yum install nano
+sudo nano /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# containerd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
+#EPEL
+sudo yum install epel-release
+sudo yum repolist
+sudo yum repolist | grep epel
+# if needed to enable epel sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E '%{rhel}').noarch.rpm
+
+ sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+# sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
+
+# cgroup driver
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo nano /etc/containerd/config.toml
+            SystemdCgroup = true
+sudo systemctl restart containerd
+
+
+lsmod | grep br_netfilter
+# sudo modprobe br_netfilter
+
+# enable routes 
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# check ports
+sudo yum install lsof netstat
+sudo lsof -i -P -n | grep LISTEN
+sudo netstat -tulpn | grep LISTEN
+sudo lsof -i:6443
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+VERSION=1.20.1
+sudo yum install -y kubelet-$VERSION kubeadm-$VERSION kubectl-$VERSION --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+sudo systemctl status kubelet
+
+sudo yum install yum-plugin-versionlock
+sudo yum versionlock kubelet-$VERSION kubeadm-$VERSION kubectl-$VERSION
+sudo yum update
+
+sudo systemctl status kubelet.service 
+sudo systemctl status containerd.service 
+
+
+#Ensure both are set to start when the system starts up.
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
+
+# c1-cp1
 # Open port with firwalld
 curl https://10.0.2.8:6443
 # firewalld should be installed
@@ -20,10 +141,16 @@ sudo firewall-cmd --list-all
 #   icmp-blocks: 
 #   rich rules:
 kubeadm token create --print-join-command
+# c1-node1
 #PASTE_JOIN_COMMAND_HERE be sure to add sudo
 sudo kubeadm join 172.16.94.10:6443 \
   --token xguxr9.zungfo8srvsxwk3h     \
   --discovery-token-ca-cert-hash sha256:0735b1db947bcdc68e01feb38d9f1e16a02d26251c95908576ea2be31cd14946 
+
+
+# --- 
+
+
 
 
 #On c1-cp1 - if you didn't keep the output, on the Control Plane Node, you can get the token.
